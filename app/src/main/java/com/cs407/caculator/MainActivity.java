ec7bc27c0f2e608ba3eb4a49267ea1c376290664
Jelly <ryuan28@wisc.edu>
2023-09-24 10:58:16 -0500

    update

--- app/src/main/java/com/cs407/caculator/MainActivity.java
+++ app/src/main/java/com/cs407/caculator/MainActivity.java
@@ -4,9 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    String s;
+    public void clickFunctionplus(View view){
+        EditText input1 = (EditText) findViewById(R.id.myTextField1);
+        EditText input2 = (EditText) findViewById(R.id.myTextField2);
+        s = input1.toString() + input2.toString();
+        goToActivity(s);
+    }
 
+    public void clickFunctionminus(View view){
+        EditText input1 = (EditText) findViewById(R.id.myTextField1);
+        EditText input2 = (EditText) findViewById(R.id.myTextField2);
+        //s = Float.parseFloat(input1.toString()) - Float.parseFloat(input2.toString());
+        goToActivity(s);
+    }
 
     public void goToActivity(String s){
         Intent intent = new Intent(this, cal.class);
