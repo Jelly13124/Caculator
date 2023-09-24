a15df15d14f12549a66b3c0f0a34c9eb31623e95
Jelly <ryuan28@wisc.edu>
2023-09-24 10:44:02 -0500

    update

--- app/src/main/java/com/cs407/caculator/MainActivity.java
+++ app/src/main/java/com/cs407/caculator/MainActivity.java
@@ -2,10 +2,17 @@ package com.cs407.caculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public void goToActivity(String s){
+        Intent intent = new Intent(this, cal.class);
+        intent.putExtra("result",s);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/cs407/caculator/cal.java
+++ app/src/main/java/com/cs407/caculator/cal.java
@@ -2,13 +2,19 @@ package com.cs407.caculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class cal extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cal);
+        textView =(TextView) findViewById(R.id.textView);
+        Intent intent=getIntent();
+        String str=intent.getStringExtra("result");
+        textView.setText(str);
     }
 }
